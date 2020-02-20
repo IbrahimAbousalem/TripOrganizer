eeb37deb6955a345fb5be516099997ca226abceb
Abd-ElMalek Gamal Hassan <malekroshdi@gmail.com>
2020-02-20 14:43:26 +0200

    CRUD operations on Firestore added/modified

--- app/src/main/java/com/iti/mobile/triporganizer/data/firebase/NotesFirebase.java
+++ app/src/main/java/com/iti/mobile/triporganizer/data/firebase/NotesFirebase.java
@@ -1,4 +1,48 @@
 package com.iti.mobile.triporganizer.data.firebase;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.iti.mobile.triporganizer.data.entities.Note;
+import com.iti.mobile.triporganizer.utils.FirestoreConstatnts;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.iti.mobile.triporganizer.utils.FirestoreConstatnts.notesReference;
+import static com.iti.mobile.triporganizer.utils.FirestoreConstatnts.tripsReference;
+
 public class NotesFirebase {
+
+    public boolean addNote(Note note) {
+        DocumentReference reference = notesReference.document();
+        note.setId(reference.getId());
+        return reference.set(note).isSuccessful();
+    }
+
+    public boolean deleteNote(Note note) {
+        DocumentReference reference = notesReference.document(note.getId());
+        return reference.delete().isSuccessful();
+    }
+
+    public boolean updateNote(Note note) {
+        DocumentReference reference = tripsReference.document(note.getId());
+        Map<String, Object> mTrip = new HashMap<>();
+        mTrip.put(FirestoreConstatnts.id, reference.getId());
+        mTrip.put(FirestoreConstatnts.message, note.getMessage());
+        mTrip.put(FirestoreConstatnts.tripId, note.getTripId());
+        mTrip.put(FirestoreConstatnts.status, note.getStatus());
+        return reference.update(mTrip).isSuccessful();
+    }
+
+    public LiveData<List<Note>> getNotesForTrip(String tripId) {
+        MutableLiveData<List<Note>> listLiveData = new MutableLiveData<>();
+        tripsReference.whereEqualTo(FirestoreConstatnts.tripId,tripId).get().addOnCompleteListener(task -> {
+            List<Note> notes = task.getResult().toObjects(Note.class);
+            listLiveData.postValue(notes);
+        });
+        return listLiveData;
+    }
 }
--- app/src/main/java/com/iti/mobile/triporganizer/data/repository/trips/TripRepositoryImp.java
+++ app/src/main/java/com/iti/mobile/triporganizer/data/repository/trips/TripRepositoryImp.java
@@ -1,77 +1,35 @@
 package com.iti.mobile.triporganizer.data.repository.trips;
 
-import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.QuerySnapshot;
 import com.iti.mobile.triporganizer.data.entities.Trip;
-import com.iti.mobile.triporganizer.utils.FirestoreConstatnts;
-
-import java.util.HashMap;
+import com.iti.mobile.triporganizer.data.firebase.TripsFirebase;
 import java.util.List;
-import java.util.Map;
-
-import static com.iti.mobile.triporganizer.utils.FirestoreConstatnts.tripsReference;
 
 public class TripRepositoryImp implements TripsRepository {
+    TripsFirebase tripsFirebase;
+
+    public TripRepositoryImp(TripsFirebase tripsFirebase){
+        this.tripsFirebase = tripsFirebase;
+    }
 
     @Override
     public boolean addTrip(Trip trip) {
-        DocumentReference reference = tripsReference.document();
-        trip.setId(reference.getId());
-        /*
-        Map<String, Object> mTrip = new HashMap<>();
-        mTrip.put(FirestoreConstatnts.id, reference.getId());
-        mTrip.put(FirestoreConstatnts.type, trip.getType());
-        mTrip.put(FirestoreConstatnts.date, trip.getDate());
-        mTrip.put(FirestoreConstatnts.endPoint, trip.getEndPoint());
-        mTrip.put(FirestoreConstatnts.isRound, trip.isRound());
-        mTrip.put(FirestoreConstatnts.userId, trip.getUserId());
-        mTrip.put(FirestoreConstatnts.startPoint, trip.getStartPoint());
-        mTrip.put(FirestoreConstatnts.roundTrip, trip.getRoundTrip());
-        mTrip.put(FirestoreConstatnts.status, trip.getStatus());
-         */
-        return reference.set(trip).isSuccessful();
+      return tripsFirebase.addTrip(trip);
 
     }
 
     @Override
     public boolean deleteTrip(Trip trip) {
-        DocumentReference reference = tripsReference.document(trip.getId());
-        return reference.delete().isSuccessful();
+       return tripsFirebase.deleteTrip(trip);
     }
 
     @Override
     public boolean updateTrip(Trip trip) {
-        DocumentReference reference = tripsReference.document(trip.getId());
-        Map<String, Object> mTrip = new HashMap<>();
-        mTrip.put(FirestoreConstatnts.id, reference.getId());
-        mTrip.put(FirestoreConstatnts.type, trip.getType());
-        mTrip.put(FirestoreConstatnts.date, trip.getDate());
-        mTrip.put(FirestoreConstatnts.endPoint, trip.getEndPoint());
-        mTrip.put(FirestoreConstatnts.isRound, trip.isRound());
-        mTrip.put(FirestoreConstatnts.userId, trip.getUserId());
-        mTrip.put(FirestoreConstatnts.startPoint, trip.getStartPoint());
-        mTrip.put(FirestoreConstatnts.roundTrip, trip.getRoundTrip());
-        mTrip.put(FirestoreConstatnts.status, trip.getStatus());
-        return reference.update(mTrip).isSuccessful();
+       return tripsFirebase.updateTrip(trip);
     }
 
     @Override
     public LiveData<List<Trip>> getTripsForUser(String userId) {
-        MutableLiveData<List<Trip>> listLiveData = new MutableLiveData<>();
-        tripsReference.whereEqualTo(FirestoreConstatnts.userId,userId).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                List<Trip> trips = task.getResult().toObjects(Trip.class);
-                listLiveData.postValue(trips);
-            }
-        });
-        return listLiveData;
+       return tripsFirebase.getTripsForUser(userId);
     }
 }
