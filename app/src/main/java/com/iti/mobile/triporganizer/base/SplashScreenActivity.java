21e23497203bb3100092e8ba79fae24d2041a5a1
eman <emankamalnasr96@gmail.com>
2020-03-10 11:28:24 +0200

    splash screen is added

---
+++ app/src/main/java/com/iti/mobile/triporganizer/base/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.iti.mobile.triporganizer.base;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModel;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.iti.mobile.triporganizer.R;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EasySplashScreen easySplashScreenView = new EasySplashScreen(SplashScreenActivity.this)
+                .withFullScreen()
+                .withTargetActivity(MainActivity.class)
+                .withSplashTimeOut(4000)
+                .withBackgroundResource(android.R.color.white)
+                .withLogo(R.drawable.ic_logo);
+
+        View view=easySplashScreenView.create();
+        setContentView(view);
+    }
+}
